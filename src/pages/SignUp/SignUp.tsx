cee2c613a26276e63fe1c3153734685adec2ebe2
Juan David Esteves Giratá <juan.esteves@jalasoft.com>
2023-02-02 16:57:34 -0500

    Refactor of the form with Material UI

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,27 +1,112 @@
-import { useField } from "@/hooks";
+import { SignUp as SignUpModel} from "@/models";
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {
+	Box,
+	Button,
+	Container,
+	Grid,
+	IconButton,
+	InputAdornment,
+	OutlinedInput,
+	Paper,
+	TextField,
+	Typography
+} from "@mui/material";
+import { useState } from "react";
 
 export interface SignUpInterface {}
 
 const SignUp : React.FC<SignUpInterface> = () => {
-	const username = useField('text', 'username', 'Username')
-	const password = useField('password', 'password', 'Password')
+	const [loginData, setLoginData] = useState<SignUpModel>({
+		Username: "",
+		Password: "",
+		Email: "",
+	  });	  
+	const [showPassword, setShowPassword] = useState(false);
 
-	const handleSubmit = (e: React.SyntheticEvent) => {	
-        e.preventDefault();
-		console.log(username);
-    }	
+  	const handleClickShowPassword = () => setShowPassword((show) => !show);
+	
+	const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+		event.preventDefault();
+	  };
+	
+	  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setLoginData({ ...loginData, [e.target.name]: e.target.value });
+	  };
+	
+	  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+		e.preventDefault();
+	  };
+	  
 	return (
-		<form onClick={(event)=> handleSubmit(event)}>
-			<div>
-				<input {...username}/>
-			</div>
-			<div>
-				<input {...password}/>
-			</div>
-			<div>
-				<button type='submit'>Send</button>'
-			</div>
-		</form>
+		<Container maxWidth="sm">
+			<Grid
+				container
+				direction="column"
+				alignItems="center"
+				justifyContent="center"
+				sx={{ minHeight: "90vh" }}
+			>
+				<Grid item>
+					<Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+						<Typography sx={{ mt: 1, mb: 1 }} variant="h4">
+							Sign Up
+						</Typography>
+						<Box component="form" onSubmit={handleSubmit}>
+							<TextField
+								name="username"
+								margin="normal"
+								type="text"
+								fullWidth
+								label="Username"
+								sx={{ mt: 2, mb: 1.5 }}
+								required
+								onChange={dataLogin}
+							/>          
+							<TextField
+								name="email"
+								margin="normal"
+								type="text"
+								fullWidth
+								label="Email"
+								sx={{ mt: 2, mb: 1.5 }}
+								required
+								onChange={dataLogin}
+							/>          
+							<OutlinedInput
+								id="outlined-adornment-password"
+								type={showPassword ? 'text' : 'password'}
+								endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+									aria-label="toggle password visibility"
+									onClick={handleClickShowPassword}
+									onMouseDown={handleMouseDownPassword}
+									edge="end"
+									>
+									{showPassword ? <VisibilityOff /> : <Visibility />}
+									</IconButton>
+								</InputAdornment>
+								}
+								sx={{ mt: 1.5, mb: 1.5 }}
+								label="Password"
+								fullWidth
+								required						
+							/>					
+							<Button
+								fullWidth
+								type="submit"
+								variant="contained"
+								sx={{ mt: 1.5, mb: 3 }}
+							>
+							Sign Up
+							</Button>
+						</Box>
+			  		</Paper>
+				</Grid>
+		  	</Grid>
+		</Container>
 	);
 }
 
